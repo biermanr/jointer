892f5b5b9fa01934a18119ca95cf5ff2c6058124
Rob Bierman <rbierman@princeton.edu>
2023-11-06 16:49:20 -0500

    starting to implement count merge

--- src/count_file.rs
+++ src/count_file.rs
@@ -1,12 +1,12 @@
 use std::cmp::Ordering;
-use std::fs;
-use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use std::fs;
+use std::io;
 
-pub struct CountFile {
-    f: io::Lines<io::BufReader<fs::File>>,
-    chrom: String,
+#[derive(PartialEq,Debug)]
+struct CountData {
+    chrom: String, //NOTE handle the chromosomes better! new struct?
     pos: u32,
     ref_base: char,
     alt_base: char,
@@ -20,38 +20,65 @@ pub struct CountFile {
     alt_fwd: u16,
     ref_rev: u16,
     alt_rev: u16,
-    exhausted: bool,
+}
+
+pub struct CountFile {
+    f: io::Lines<io::BufReader<fs::File>>,
+    data: CountData,
 }
 
 impl CountFile {
     pub fn new(count_path: &str) -> CountFile {
+        let mut f = Self::read_lines(count_path).expect(
+            "Error, could not read file");
 
-        CountFile {
-            f : Self::read_lines(count_path).expect(
-                "Error, could not read file "),
-            chrom : String::new(),
-            pos : 0,
-            ref_base : 'N',
-            alt_base : 'N',
-            bef_base : 'N',
-            aft_base : 'N',
-            ref_count : 0,
-            alt_count : 0,
-            ref_indel : 0,
-            alt_indel : 0,
-            ref_fwd : 0,
-            alt_fwd : 0,
-            ref_rev : 0,
-            alt_rev : 0,
-            exhausted : false,
-        }
-        //next???
+        f.next(); //skip header
+
+        //NOTE handle this better
+        let line = f.next().unwrap().expect(
+            "Error, could not read line");
+
+        let data = Self::parse_line(&line);
+        
+        CountFile{f:f, data:data}
     }
 
-    //NOTE
-    //pub fn next(&self) {
-        //???
-    //}
+    ///Read the next line and update fields
+    ///updates the CountData
+    ///returns true if successful, otherwise false
+    pub fn next(&mut self) -> bool {
+        if let Some(Ok(line)) = self.f.next() {
+            self.data = Self::parse_line(&line);
+            true
+        } else {
+            false
+        }
+    }
+
+    ///Parse the count file line into a CountData object
+    fn parse_line(line: &str) -> CountData {
+        let parts: Vec<&str> = line
+            .split('\t')
+            .take(14)
+            .collect();
+
+        CountData {
+            chrom: parts[0].into(),
+            pos: parts[1].parse().unwrap(),
+            ref_base: parts[2].chars().next().unwrap(),
+            alt_base: parts[3].chars().next().unwrap(),
+            bef_base: parts[4].chars().next().unwrap(),
+            aft_base: parts[5].chars().next().unwrap(),
+            ref_count: parts[6].parse().unwrap(),
+            alt_count: parts[7].parse().unwrap(),
+            ref_indel: parts[8].parse().unwrap(),
+            alt_indel: parts[9].parse().unwrap(),
+            ref_fwd: parts[10].parse().unwrap(),
+            alt_fwd: parts[11].parse().unwrap(),
+            ref_rev: parts[12].parse().unwrap(),
+            alt_rev: parts[13].parse().unwrap(),
+        }
+    }
 
     ///Create buffered line reader of a file
     fn read_lines<P>(fname: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
@@ -64,8 +91,8 @@ impl CountFile {
 
 impl Ord for CountFile {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.chrom.cmp(&other.chrom)
-            .then_with(|| self.pos.cmp(&other.pos))
+        self.data.chrom.cmp(&other.data.chrom)
+            .then_with(|| self.data.pos.cmp(&other.data.pos))
     }
 }
 
@@ -77,10 +104,40 @@ impl PartialOrd for CountFile {
 
 impl PartialEq for CountFile {
     fn eq(&self, other: &Self) -> bool {
-        self.chrom.eq(&other.chrom) && self.pos.eq(&other.pos)
+        self.data.chrom.eq(&other.data.chrom) && self.data.pos.eq(&other.data.pos)
     }
 }
 
 impl Eq for CountFile {}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn test_parse_line() {
+        let line = "chr22	10513926	A	.	A	A	1	0	0	0	1	0	0	0	.	.	.	.	.	.	.	.	.	.";
+        let cf = CountFile::parse_line(line);
+
+        let ground_truth = CountData {
+            chrom: "chr22".into(),
+            pos: 10513926,
+            ref_base: 'A',
+            alt_base: '.',
+            bef_base: 'A',
+            aft_base: 'A',
+            ref_count: 1,
+            alt_count: 0,
+            ref_indel: 0,
+            alt_indel: 0,
+            ref_fwd: 1,
+            alt_fwd: 0,
+            ref_rev: 0,
+            alt_rev: 0,
+        };
+
+        assert_eq!(cf,ground_truth);
+    }
+
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,42 @@ use std::collections::BinaryHeap;
 
 /// Merge counts files
 #[pyfunction]
-fn rs_merge(count_paths: Vec<&str>, out_path: &str) {
+fn rs_merge(count_paths: Vec<&str>, _out_path: &str) {
     let mut cfs: BinaryHeap<CountFile> = count_paths
         .iter()
         .map(|x| CountFile::new(x))
         .collect();
 
-    
+    while cfs.len() > 0 {
+        //the first CountFile position will be the min
+        let min_cf = cfs.pop().unwrap();
+        let mut curr_cfs: Vec<CountFile> = Vec::new();
+
+        //keep popping until a greater value is found
+        while cfs.len() > 0 {
+            let cf = cfs.pop().unwrap();
+            if cf <= min_cf {
+                curr_cfs.push(cf);
+            } else {
+                cfs.push(cf);
+            }
+        }
+        curr_cfs.push(min_cf);
+
+        //Aggregate the CountFile objects to prepare for outpu
+        //NOTE
+        
+
+        //Write the output to the out_path
+        //NOTE
+
+        //add back to the BinaryHeap if not spent
+        for mut cf in curr_cfs {
+            if cf.next() {
+                cfs.push(cf);
+            }
+        }
+    }
 
 }
 
