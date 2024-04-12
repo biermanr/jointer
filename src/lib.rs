e05ef484663a3b146168fd74c6679aedaf3d9b8c
Rob Bierman <rbierman@princeton.edu>
2024-04-12 10:53:22 -0400

    refact: Chat-GPT helped refact inner_join. - `while let` was the key along with peek

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,34 @@
 use pyo3::prelude::*;
 use std::fs::File;
-use std::io::{self, BufRead, Lines, BufReader, Write};
+use std::io::{self, BufRead, BufReader, Write};
 use std::path::Path;
 
 #[pyfunction]
 fn join(file_path1: &str, file_path2: &str) {
-    inner_join(file_path1, file_path2, io::stdout());
+    inner_join(Path::new(file_path1), Path::new(file_path2), io::stdout());
 }
 
-fn inner_join(file_path1: &str, file_path2: &str, mut out: impl Write) {
-    let mut f1_lines = read_lines(file_path1).expect("Could not read lines from file 1");
-    let mut f2_lines = read_lines(file_path2).expect("Could not read lines from file 2");
+fn inner_join(file_path1: &Path, file_path2: &Path, mut out: impl Write) {
+    let file1 = File::open(file_path1).expect("Could not open file 1");
+    let file2 = File::open(file_path2).expect("Could not open file 2");
 
-    let mut f1_line = f1_lines.next().expect("Could not iterate file1").unwrap();
-    let mut f2_line = f2_lines.next().expect("Could not iterate file2").unwrap();
+    let mut lines1 = BufReader::new(file1).lines().peekable();
+    let mut lines2 = BufReader::new(file2).lines().peekable();
 
-    let (mut f1_key,  mut _f1_rest) = f1_line.split_once("\t").expect("Could not split line in f1");
-    let (mut f2_key,  mut  f2_rest) = f2_line.split_once("\t").expect("Could not split line in f2");
+    while let (Some(line1), Some(line2)) = (lines1.peek(), lines2.peek()) {
+        let (key1, rest1) = line1.as_ref().expect("Error reading line in file 1")
+            .split_once("\t").expect("Could not split line in file 1");
+        let (key2, rest2) = line2.as_ref().expect("Error reading line in file 2")
+            .split_once("\t").expect("Could not split line in file 2");
 
-    loop {
-
-        if f1_key == f2_key {
-            writeln!(out,"{}\t{}",f1_line, f2_rest).unwrap();
-        }
-
-        //Advance the iterators or break the loop if exhuasted
-        let advance_f1 = f1_key <= f2_key;
-        let advance_f2 = f2_key <= f1_key;
-
-        if advance_f1 {
-            f1_line = match f1_lines.next() {
-                Some(Ok(l)) => l,
-                _ => break,
-            };
-            (f1_key, _f1_rest) = f1_line.split_once("\t").expect("Could not split line in f1");
-        }
-
-        if advance_f2 {
-            f2_line = match f2_lines.next() {
-                Some(Ok(l)) => l,
-                _ => break,
-            };
-            (f2_key,  f2_rest) = f2_line.split_once("\t").expect("Could not split line in f2");
+        if key1 == key2 {
+            writeln!(out, "{}\t{}\t{}", key1, rest1, rest2).expect("Error writing output");
+            lines1.next();
+            lines2.next();
+        } else if key1 < key2 {
+            lines1.next();
+        } else {
+            lines2.next();
         }
     }
 }
@@ -50,13 +37,6 @@ fn inner_join(file_path1: &str, file_path2: &str, mut out: impl Write) {
 fn rust_utils(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(join, m)?)?;
     Ok(())
-}
-
-//Buffered read lines copied from "Rust By Example"
-fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(BufReader::new(file).lines())
 }
 
 #[cfg(test)]
@@ -73,7 +53,7 @@ mod tests {
         writeln!(country_codes_file, "CHN\tChina").unwrap();
         writeln!(country_codes_file, "USA\tUnited-States").unwrap();
         country_codes_file.flush().unwrap();
-        let country_codes_path = country_codes_file.path().to_str().unwrap();
+        let country_codes_path = country_codes_file.path();
 
         let mut country_populations_file = NamedTempFile::new().unwrap();
         writeln!(country_populations_file, "country_code\tpopulation").unwrap();
@@ -81,7 +61,7 @@ mod tests {
         writeln!(country_populations_file, "IND\t1380").unwrap();
         writeln!(country_populations_file, "USA\t328").unwrap();
         country_populations_file.flush().unwrap();
-        let country_populations_path = country_populations_file.path().to_str().unwrap();
+        let country_populations_path = country_populations_file.path();
 
         let expected_output = "country_code\tcountry_name\tpopulation\nBRA\tBrazil\t209\nUSA\tUnited-States\t328\n";
 
